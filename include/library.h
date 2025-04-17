01b71c58f649e6b98b9bdbd150fd7ad110838ea0
enricostara <enrico.stara@gmail.com>
2025-04-17 22:53:04 +0200

    Restructure project: move sources to src/, headers to include/, update CMakeLists.txt, and clean up layout

---
+++ include/library.h
@@ -0,0 +1,6 @@
+#ifndef CLERIC_LIBRARY_H
+#define CLERIC_LIBRARY_H
+
+void hello(void);
+
+#endif //CLERIC_LIBRARY_H
---
+++ src/library.c
@@ -0,0 +1,12 @@
+#include "../include/library.h"
+
+#include <stdio.h>
+
+void hello(void) {
+    printf("Hello, World!\n");
+}
+
+int main(int argc, char *argv[]) {
+    hello();
+    return 0;
+}
