2d0a418aa5a5b9c7299b86b53dd851ed806e749b
enricostara <enrico.stara@gmail.com>
2025-04-26 00:26:00 +0200

    style(codegen): Apply formatting and update comments

--- src/codegen/codegen.h
+++ src/codegen/codegen.h
@@ -8,10 +8,10 @@
 /**
  * Generates assembly code (x86-64 macOS AT&T syntax) from the AST.
  *
- * @param state Pointer to the CodegenState containing the output buffer.
+ * @param sb Pointer to the StringBuffer to store the generated assembly code.
  * @param program The root node of the Abstract Syntax Tree.
  * @return true if code generation was successful, false otherwise.
  */
-bool codegen_generate_program(StringBuffer buffer, ProgramNode *program);
+bool codegen_generate_program(StringBuffer *sb, ProgramNode *program);
 
 #endif // CODEGEN_H
